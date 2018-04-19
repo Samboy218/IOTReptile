7e4f26f6456404b1e901618c2c8e1df29965fe50
Samuel Wehunt <sam.wehunt@gmail.com>
2018-04-18 21:39:50 -0500

    added configuration file for setting up pins

--- daemon/src/dhtd.c
+++ daemon/src/dhtd.c
@@ -3,12 +3,16 @@
 #include <unistd.h>
 #include "dht22.h"
 
-int main() {
+int main(int argc, char** argv) {
     pid_t pid, sid;
 
     if (geteuid()) {
         //must be root to write to necessary files
         //in the future we should find a way to run the daemon as non-root
+        exit(EXIT_FAILURE);
+    }
+    if (argc < 2 || argc > 41) {
+        //gotta pass the pin numbers as parameters
         exit(EXIT_FAILURE);
     }
     pid = fork();
@@ -45,18 +49,29 @@ int main() {
 
     //placeholder values, need to properly set this up
     long int curr_time;
-    int num_sense = 1;
-    int gpio_pin[1];
-    gpio_pin[0] = 7;
-    float humidity[1];
-    float temperature[1];
     int i;
+    int num_sense = argc - 1;
+    float humidity;
+    float temperature;
+    //because there aren't more than 40 pins
+    int gpio_pin[40];
+    for (i=0; i < num_sense; i++) {
+        gpio_pin[i] = atoi(argv[i+1]);
+    }
+
     while(1) {
         curr_time = (long int)time(NULL);
         for (i = 0; i < num_sense; i++) {
-            if (read_dht_data(gpio_pin[i], &humidity[i], &temperature[i]))
-                fprintf(log, "%d, %d, %3.3f, %3.3f\n", 
-                              curr_time, i, humidity[i], temperature[i]);
+            //try 20 times
+            int num_tries = 20;
+            while (num_tries > 0) {
+                if (read_dht_data(gpio_pin[i], &humidity, &temperature)) {
+                    fprintf(log, "%d, %d, %3.3f, %3.3f\n", 
+                              curr_time, i, humidity, temperature);
+                    break;
+                }
+                num_tries--;
+            }
         }
         fflush(log);
         sleep(60);
