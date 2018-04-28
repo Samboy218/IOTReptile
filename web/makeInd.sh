c2e32ca6b62fa3a256102e95ffe0cacf91783cb8
Samuel Wehunt <sam.wehunt@gmail.com>
2018-04-27 21:41:28 -0500

    one too many links to sensors

--- web/makeInd.sh
+++ web/makeInd.sh
@@ -5,6 +5,7 @@ echo "<!DOCTYPE html>"
 echo "<html>"
 echo "<head><title>Temperature Sensor Console</title></head>"
 echo "<body>"
+(($1--))
 for i in `seq 0 $1`;
 do
     echo "<a href=/sens${i}/>Sensor ${i}</a><br />"
